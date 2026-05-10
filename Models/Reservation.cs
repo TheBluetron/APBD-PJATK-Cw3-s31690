321465eaf94e2adbc61d25a82162f3a21b16e78f
Patron125 <73890986+Patron125@users.noreply.github.com>
2026-05-10 16:00:40 +0200

    Reservation

--- Models/Reservation.cs
+++ Models/Reservation.cs
@@ -2,5 +2,12 @@
 
 public class Reservation
 {
-    
+    public int id { get; set; }
+    public int roomID { get; set; }
+    public string organizerName { get; set; }
+    public string topic { get; set; }
+    public DateTime date { get; set; }
+    public DateTime startTime { get; set; }
+    public DateTime endTime { get; set; }
+    public string status { get; set; } // planned, confirmed, cancelled
 }
